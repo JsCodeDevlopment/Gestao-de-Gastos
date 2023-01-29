788bc18b43b9ba7d44e706dff1d18862618d7f32
Jonata Silva <jonatasilva118@gmail.com>
2023-01-29 16:27:28 -0300

    add funcionalidade de atribuição

---
+++ index.js
@@ -0,0 +1,31 @@
+const inputName = document.querySelector("#nameInput")
+const inputValue = document.querySelector("#numberInput")
+const addButton = document.querySelector("#addButton")
+const divReceptora = document.querySelector(".transacoesAdicionadas")
+
+addButton.addEventListener("click", ()=>{
+    let divCard = document.createElement("div");
+    divCard.className = "card"
+    divReceptora.appendChild(divCard)
+    let pName = document.createElement("p");
+    pName.className = "mainFont"
+    pName.innerText = `${inputName.value}`
+    divCard.append(pName)
+    let divPriceAndTrash = document.createElement("div");
+    divPriceAndTrash.className = "priceAndTrash"
+    divCard.append(divPriceAndTrash)
+    let pValue = document.createElement("p");
+    const currencyFormatter = new Intl.NumberFormat(undefined, { style: "currency", currency: "BRL" })
+    pValue.className = "mainFont"
+    pValue.innerText = currencyFormatter.format(inputValue.value)
+    divPriceAndTrash.append(pValue)
+    let trashImage = document.createElement("img");
+    trashImage.src = "Imagens/LIXEIRA FECHADA.png"
+    trashImage.width = "14"
+    trashImage.height = "18"
+    divPriceAndTrash.append(trashImage)
+
+    
+
+    console.log(divReceptora)
+})
