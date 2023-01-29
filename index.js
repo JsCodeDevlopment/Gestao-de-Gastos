c1907ea896af47a5d9df3a1e2aa77e56ac596163
Jonata Silva <jonatasilva118@gmail.com>
2023-01-29 17:42:50 -0300

    updates

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ addButton.addEventListener("click", ()=>{
     let divCard = document.createElement("div");
     divCard.className = "card"
     divReceptora.appendChild(divCard)
+
     let pName = document.createElement("p");
     pName.className = "mainFont"
     pName.innerText = `${inputName.value}`
     divCard.append(pName)
+
     let divPriceAndTrash = document.createElement("div");
     divPriceAndTrash.className = "priceAndTrash"
     divCard.append(divPriceAndTrash)
+
     let pValue = document.createElement("p");
     const currencyFormatter = new Intl.NumberFormat(undefined, { style: "currency", currency: "BRL" })
     pValue.className = "mainFont"
     pValue.innerText = currencyFormatter.format(inputValue.value)
     divPriceAndTrash.append(pValue)
+
     let trashImage = document.createElement("img");
     trashImage.src = "Imagens/LIXEIRA FECHADA.png"
     trashImage.width = "14"
@@ -26,7 +30,17 @@ addButton.addEventListener("click", ()=>{
     trashImage.id = "trash"
     divPriceAndTrash.append(trashImage)
 
-    
+    const atualTrashImage = document.querySelector("#trash")
+    let atualTrash = "Imagens/LIXEIRA FECHADA.png";
+    trashImage.addEventListener("mouseover", ()=>{
+        if (atualTrash === "Imagens/LIXEIRA FECHADA.png") {
+            atualTrash = "Imagens/LIXEIRA ABERTA.png";
+            atualTrashImage.setAttribute("src", "Imagens/LIXEIRA ABERTA.png");
+          } else {
+            atualTrash = "Imagens/LIXEIRA ABERTA.png";
+            atualTrashImage.setAttribute("src", "Imagens/LIXEIRA FECHADA.png");
+          }
+    })
 
     console.log(divReceptora)
 })
