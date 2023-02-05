c118b192fa014836a29d2cf86b51a94b01241b07
Jonata Silva <jonatasilva118@gmail.com>
2023-02-05 17:35:54 -0300

    Starting dark mode

--- Modules/animations.js
+++ Modules/animations.js
@@ -29,28 +29,28 @@ export function deleteInputValues () {
 
 //Create Light Mode
 // Changes
-const darkMode = document.getElementById("#darkModeButton")
-console.log(darkMode)
+// const darkMode = document.getElementById("#darkModeButton")
 
-const navegation = document.querySelector(".navegation")
-navegation.style.backgroundColor = "#fff"
-const mainContainer = document.querySelector(".mainContainer")
-mainContainer.style.backgroundColor = "#e8f5f6"
-const contentContainer = document.querySelector(".container")
-contentContainer.style.backgroundColor = "#fff"
-const footer = document.querySelector("footer")
-footer.style.backgroundColor = "#fff"
-const inputName = document.querySelector("#nameInput")
-inputName.style.backgroundColor = "#fff"
-inputName.style.color = "#000"
-const inputValue = document.querySelector("#numberInput")
-inputValue.style.backgroundColor = "#fff"
-inputValue.style.color = "#000"
-const littleFont = document.querySelectorAll(".littleFont")
-console.log(littleFont)
-littleFont[0].style.color = "#000"
-littleFont[1].style.color = "#000"
-// const card = document.querySelector(".card")
-// card.style.backgroundColor = "#fff"
-// const texts = document.querySelector(".mainfont")
-// texts.style.color = "#000"
+
+// const navegation = document.querySelector(".navegation")
+// navegation.style.backgroundColor = "#fff"
+// const mainContainer = document.querySelector(".mainContainer")
+// mainContainer.style.backgroundColor = "#e8f5f6"
+// const contentContainer = document.querySelector(".container")
+// contentContainer.style.backgroundColor = "#fff"
+// const footer = document.querySelector("footer")
+// footer.style.backgroundColor = "#fff"
+// const inputName = document.querySelector("#nameInput")
+// inputName.style.backgroundColor = "#fff"
+// inputName.style.color = "#000"
+// const inputValue = document.querySelector("#numberInput")
+// inputValue.style.backgroundColor = "#fff"
+// inputValue.style.color = "#000"
+// const littleFont = document.querySelectorAll(".littleFont")
+// console.log(littleFont)
+// littleFont[0].style.color = "#000"
+// littleFont[1].style.color = "#000"
+// // const card = document.querySelector(".card")
+// // card.style.backgroundColor = "#fff"
+// // const texts = document.querySelector(".mainfont")
+// // texts.style.color = "#000"
