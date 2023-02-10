a2bea9554ad18470880a5185b651dd53867b15f8
Jonata Silva <jonatasilva118@gmail.com>
2023-02-09 23:26:11 -0300

    refac: modularizing the theme function

--- index.js
+++ index.js
@@ -1,147 +1,165 @@
 import { start } from "./Modules/animations.js";
 import { toggleTrashImage } from "./Modules/animations.js";
 import { deleteInputValues } from "./Modules/animations.js";
+import { toggleTheme } from "./Modules/theme.js";
 
-start()
+start();
 
 let indiceCard = 0;
 
-const currencyFormatter = new Intl.NumberFormat(undefined, { style: "currency", currency: "BRL" })
+const currencyFormatter = new Intl.NumberFormat(undefined, {
+  style: "currency",
+  currency: "BRL",
+});
 
-const inputName = document.querySelector("#nameInput")
-const inputValue = document.querySelector("#numberInput")
-const addButton = document.querySelector("#addButton")
-const divReceptora = document.querySelector(".transacoesAdicionadas")
-const pValorReceitas = document.querySelector("#green")
-const pValorDespesas = document.querySelector("#red")
-const pSaldoAtual = document.querySelector(".titleFontValue")
+const inputName = document.querySelector("#nameInput");
+const inputValue = document.querySelector("#numberInput");
+const addButton = document.querySelector("#addButton");
+const divReceptora = document.querySelector(".transacoesAdicionadas");
+const pValorReceitas = document.querySelector("#green");
+const pValorDespesas = document.querySelector("#red");
+const pSaldoAtual = document.querySelector(".titleFontValue");
 
-pSaldoAtual.innerText = "R$ 0,00"
-pValorReceitas.innerText = "R$ 0,00"
-pValorDespesas.innerText = "R$ 0,00"
+pSaldoAtual.innerText = "R$ 0,00";
+pValorReceitas.innerText = "R$ 0,00";
+pValorDespesas.innerText = "R$ 0,00";
 
 let positiveValues = [];
 let negativeValues = [];
 
-addButton.addEventListener("click", ()=>{
-  
+addButton.addEventListener("click", () => {
   if (inputName.value === "" || inputValue.value === "") {
-    alert("Preencha os campos!")
-    return
+    alert("Preencha os campos!");
+    return;
   }
 
-    indiceCard ++
+  indiceCard++;
 
-    const divCard = document.createElement("div");
-    divCard.className = "card"
-    divCard.id = "card-" + indiceCard;
-    divReceptora.appendChild(divCard)
+  const divCard = document.createElement("div");
+  divCard.className = "card";
+  divCard.id = "card-" + indiceCard;
+  divReceptora.appendChild(divCard);
 
-    const pName = document.createElement("p");
-    pName.className = "mainFont"
-    pName.innerText = `${inputName.value}`
-    divCard.append(pName)
+  const pName = document.createElement("p");
+  pName.className = "mainFont";
+  pName.innerText = `${inputName.value}`;
+  divCard.append(pName);
 
-    const divPriceAndTrash = document.createElement("div");
-    divPriceAndTrash.className = "priceAndTrash"
-    divCard.append(divPriceAndTrash)
+  const divPriceAndTrash = document.createElement("div");
+  divPriceAndTrash.className = "priceAndTrash";
+  divCard.append(divPriceAndTrash);
 
-    const pValue = document.createElement("p");
-    pValue.className = "mainFont"
-    pValue.innerText = currencyFormatter.format(inputValue.value)
-    pValue.id = "valor-" + indiceCard;
-    divPriceAndTrash.append(pValue)
+  const pValue = document.createElement("p");
+  pValue.className = "mainFont";
+  pValue.innerText = currencyFormatter.format(inputValue.value);
+  pValue.id = "valor-" + indiceCard;
+  divPriceAndTrash.append(pValue);
 
-    const editButton = document.createElement("button");
-    editButton.id = "editButton"
-    divPriceAndTrash.append(editButton)
-    const editImg = document.createElement("img");
-    editImg.src = "Imagens/EDIT-BUTTON.png"
-    editImg.width = "18"
-    editImg.height = "18"
-    editImg.classList.add("edit")
-    editButton.append(editImg)
+  const editButton = document.createElement("button");
+  editButton.id = "editButton";
+  divPriceAndTrash.append(editButton);
+  const editImg = document.createElement("img");
+  editImg.src = "Imagens/EDIT-BUTTON.png";
+  editImg.width = "18";
+  editImg.height = "18";
+  editImg.classList.add("edit");
+  editButton.append(editImg);
 
-    const deleteButton = document.createElement("button");
-    deleteButton.id = "deleteButton"
-    deleteButton.addEventListener("click", ()=>{
-      const card = deleteButton.closest(".card")
-      card.remove()
-      function removeValue (input) {
-        if (input >= 0) {
-          positiveValues = positiveValues.filter(function(el) {
-            return el !== input;
-          }); 
-        } else {
-          negativeValues = negativeValues.filter(function(el) {
-            return el !== input;})
-          }
-          updateRevenue(positiveValues)
-          updateExpense(negativeValues)
-          updateBalance(positiveValues, negativeValues)
-          
-        }
-      removeValue(input)
-    })
-
-    const trashImage = document.createElement("img");
-    trashImage.src = "Imagens/LIXEIRA FECHADA.png"
-    trashImage.width = "14"
-    trashImage.height = "18"
-    trashImage.classList.add("trash")
-    deleteButton.append(trashImage)
-  
-    divPriceAndTrash.append(deleteButton)
-    document.querySelectorAll(".card").forEach((card)=>{
-      const atualTrashImage = document.querySelector(`#${card.id} .trash`)
-      toggleTrashImage(atualTrashImage)
-    })
-    document.querySelectorAll("valor-").forEach((pValue)=>{
-      const pValueAcrecimo = document.querySelector(`#${pValue.id}`)
-    })
-    
-    function addNumber(input) {
+  const deleteButton = document.createElement("button");
+  deleteButton.id = "deleteButton";
+  deleteButton.addEventListener("click", () => {
+    const card = deleteButton.closest(".card");
+    card.remove();
+    function removeValue(input) {
       if (input >= 0) {
-        positiveValues.push(input);
+        positiveValues = positiveValues.filter(function (el) {
+          return el !== input;
+        });
       } else {
-        negativeValues.push(input);
+        negativeValues = negativeValues.filter(function (el) {
+          return el !== input;
+        });
       }
+      updateRevenue(positiveValues);
+      updateExpense(negativeValues);
+      updateBalance(positiveValues, negativeValues);
     }
-    let input = parseInt(inputValue.value)
-    addNumber(input);
+    removeValue(input);
+  });
 
-    updateRevenue(positiveValues)
-    updateExpense(negativeValues)
-    updateBalance(positiveValues, negativeValues)
-    deleteInputValues()
-    sessionStorage.setItem('positiveValues', JSON.stringify(positiveValues))
-    sessionStorage.setItem('negativeValues', JSON.stringify(negativeValues))
-  })
+  const trashImage = document.createElement("img");
+  trashImage.src = "Imagens/LIXEIRA FECHADA.png";
+  trashImage.width = "14";
+  trashImage.height = "18";
+  trashImage.classList.add("trash");
+  deleteButton.append(trashImage);
 
-  inputValue.addEventListener("keypress", (e)=>{
-    if(e.key === "e"){
-      e.preventDefault()
+  divPriceAndTrash.append(deleteButton);
+  document.querySelectorAll(".card").forEach((card) => {
+    const atualTrashImage = document.querySelector(`#${card.id} .trash`);
+    toggleTrashImage(atualTrashImage);
+  });
+  document.querySelectorAll("valor-").forEach((pValue) => {
+    const pValueAcrecimo = document.querySelector(`#${pValue.id}`);
+  });
+
+  function addNumber(input) {
+    if (input >= 0) {
+      positiveValues.push(input);
+    } else {
+      negativeValues.push(input);
     }
-  })
-
-  function updateBalance (positiveValues, negativeValues) {
-    const valoresPositivos = positiveValues.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    const valoresNegativos = negativeValues.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
-    const calc = valoresPositivos + valoresNegativos;
-
-    pSaldoAtual.innerText = currencyFormatter.format(calc)
   }
+  let input = parseInt(inputValue.value);
+  addNumber(input);
 
-  function updateRevenue (positiveValues) {
-    const newRevenue = positiveValues.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    pValorReceitas.innerText = currencyFormatter.format(newRevenue)
+  updateRevenue(positiveValues);
+  updateExpense(negativeValues);
+  updateBalance(positiveValues, negativeValues);
+  deleteInputValues();
+  sessionStorage.setItem("positiveValues", JSON.stringify(positiveValues));
+  sessionStorage.setItem("negativeValues", JSON.stringify(negativeValues));
+});
+
+inputValue.addEventListener("keypress", (e) => {
+  if (e.key === "e") {
+    e.preventDefault();
   }
+});
 
-  function updateExpense (negativeValues) {
-    const newExpense = negativeValues.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    pValorDespesas.innerText = currencyFormatter.format(newExpense)
-  }
+const toggleThemeButton = document.querySelector("#toggleThemeButton")
+toggleThemeButton.addEventListener("click", toggleTheme)
 
-  sessionStorage.setItem('positiveValues', JSON.stringify(positiveValues)) || [];
-  sessionStorage.setItem('negativeValues', JSON.stringify(negativeValues)) || [];
+function updateBalance(positiveValues, negativeValues) {
+  const valoresPositivos = positiveValues.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const valoresNegativos = negativeValues.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  const calc = valoresPositivos + valoresNegativos;
+
+  pSaldoAtual.innerText = currencyFormatter.format(calc);
+}
+
+function updateRevenue(positiveValues) {
+  const newRevenue = positiveValues.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  pValorReceitas.innerText = currencyFormatter.format(newRevenue);
+}
+
+function updateExpense(negativeValues) {
+  const newExpense = negativeValues.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  pValorDespesas.innerText = currencyFormatter.format(newExpense);
+}
+
+sessionStorage.setItem("positiveValues", JSON.stringify(positiveValues)) || [];
+sessionStorage.setItem("negativeValues", JSON.stringify(negativeValues)) || [];
